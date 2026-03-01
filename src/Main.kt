419380e7de6f91996a27b32d29bdb535d55bee86
Esteban A. Giménez <estebanalfons.gimenez@murciaeduca.es>
2026-03-01 22:01:15 +0100

    Implementación terminada

--- src/Main.kt
+++ src/Main.kt
@@ -1,28 +1,36 @@
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
-suspend fun main() = withContext(Dispatchers.Default){
+fun main() {
     val meteoData = datosMeteorologicos
-    val tempMaxima = async { maximaTemperatura(meteoData) }
-    val tempMinima = async { minimaTemperatura(meteoData) }
-    val promedioTemp = async { promedioTemperatura(meteoData) }
-    val humedadMaxima = async { humedadMaxima(meteoData) }
-    val humedadMinima = async { humedadMinima(meteoData) }
-    val vientoMaximo = async {maximoViento(meteoData) }
-    val vientoMinimo = async { minimoViento(meteoData) }
 
-    println("""TEMPERATURA:
-        |Mínima: ${"%.2f".format(tempMinima.await())}
+    var tempMinima = 0.0
+    var humedadMin = 0.0
+
+    runBlocking {
+        launch { tempMinima = minimaTemperatura(meteoData) }
+        launch { humedadMin = humedadMinima(meteoData) }
+    }
+    runBlocking {
+        val tempMaxima = async { maximaTemperatura(meteoData) }
+        val promedioTemp = async { promedioTemperatura(meteoData) }
+        val humedadMaxima = async { humedadMaxima(meteoData) }
+        val vientoMaximo = async { maximoViento(meteoData) }
+        val vientoMinimo = async { minimoViento(meteoData) }
+
+        println(
+            """TEMPERATURA:
+        |Mínima: ${"%.2f".format(tempMinima)}
         |Máxima: ${"%.2f".format(tempMaxima.await())}
         |Promedio: ${"%.2f".format(promedioTemp.await())}
         |---------------------------------
         |HUMEDAD:
-        |Mínima:${"%.2f".format(humedadMinima.await())}
+        |Mínima:${"%.2f".format(humedadMin)}
         |Máxima: ${"%.2f".format(humedadMaxima.await())}
         |---------------------------------
         |VIENTO:
         |Mínimo: ${vientoMinimo.await()}
         |Máximo: ${vientoMaximo.await()}
-    """.trimMargin())
+    """.trimMargin()
+        )
+    }
 }
